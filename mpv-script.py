9b4d54b7f18cf9e491b0d2edf937887dcf981bc8
Brett Barker <brett.barker@brbtechsolutions.com>
2023-05-28 18:14:07 -0400

    Updated logging method.

--- mpv-script.py
+++ mpv-script.py
@@ -1,8 +1,16 @@
 import mpv
 import time
+import logging
 
-# Set to True to enable debug output
-debug = True
+# Setup Logging
+logging.basicConfig(filename='mpv-logs/current.log', 
+                    encoding='utf-8', 
+                    level=logging.INFO, 
+                    format='%(asctime)s %(levelname)s %(message)s', 
+                    datefmt='%Y-%m-%dT%H:%M:%S%z')
+
+# Log the start of the script
+logging.info('Starting MPV Streaming Script')
 
 # Define the URL of the stream
 stream_url = ""
@@ -15,8 +23,7 @@ if stream_url == "":
         print("Stream URL cannot be empty. Exiting...")
         exit()
 
-if debug:
-    print("Stream URL: " + stream_url)
+logging.info("Stream URL: " + stream_url)
 
 # Create an MPV player instance
 player = mpv.MPV()
@@ -34,41 +41,35 @@ lastPlaybackTime = 0
 maxFrozenTime = 10
 
 # Allow stream 10 seconds to start before checking
+logging.debug("Waiting 10 seconds for stream to start...")
 time.sleep(10)
 
 # Loop indefinitely
 while True:
-    if debug:
-        print("Playback time: " + str(player.playback_time))
+    logging.debug("Playback time: " + str(player.playback_time))
     # Wait for a short time
     time.sleep(0.5)
 
     # Check if the stream is frozen
-    if debug:
-        print("Comparing last playback time: " + str(lastPlaybackTime) + " to current playback time: " + str(player.playback_time))
+    logging.debug("Comparing last playback time: " + str(lastPlaybackTime) + " to current playback time: " + str(player.playback_time))
     if lastPlaybackTime == player.playback_time:
-        if debug:
-            print("Stream is frozen. Restarting...")
+        logging.warning("Stream is frozen. Monitoring for Restart.")
         for i in range(0, maxFrozenTime):
             # Wait for a short time
             time.sleep(1)
             # Check if the stream is still frozen
             if lastPlaybackTime == player.playback_time:
-                if debug:
-                    print("Stream is still frozen. Restarting in " + str(maxFrozenTime - i) + " seconds...")
+                logging.warning("Stream is still frozen. Restarting in " + str(maxFrozenTime - i) + " seconds...")
                 if i == maxFrozenTime - 1:
-                    if debug:
-                        print("Stream is still frozen. Restarting now...")
+                    logging.warning("Stream is still frozen. Restarting now...")
                     # restart stream
                     player.play(stream_url)
                     # Sleep for 10 seconds to allow stream to start
                     time.sleep(10)
             else:
-                if debug:
-                    print("Stream is no longer frozen. Continuing...")
+                logging.warning("Stream is no longer frozen. Continuing...")
                 break
 
     # Update the last playback time
-    if debug:
-        print("Updating last playback time to: " + str(player.playback_time))
+    logging.debug("Updating last playback time to: " + str(player.playback_time))
     lastPlaybackTime = player.playback_time
